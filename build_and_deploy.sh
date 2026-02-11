85788df79bd27317aad9ee452775ba06090b2bd9
Bharath Babu R <61000612+bharath93-byte@users.noreply.github.com>
2026-02-11 14:18:35 +0530

    09_Feb

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,8 +1,8 @@
-if command -v cargo >/dev/null 2>&1; then
-    VERSION=$(cargo --version)
-    echo "✅ Cargo is installed: $VERSION"
-else
-    echo "❌ Cargo could not be found."
-    echo "Hint: If you just installed Rust, try restarting your terminal or running 'source \$HOME/.cargo/env'"
-    exit 1
-fi
+export RUSTUP_HOME=/tmp/.rustup
+export CARGO_HOME=/tmp/.cargo
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+source "/tmp/.cargo/env"
+sudo yum install -y gcc
